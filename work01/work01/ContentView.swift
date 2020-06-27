ed9d488b627a36ef24fba3ee03188db9fb351659
akanayama <atsu2424.829@gmail.com>
2020-06-27 21:48:03 +0900

    test

---
+++ work01/work01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  work01
+//
+//  Created by 金山徳志 on 2020/06/27.
+//  Copyright © 2020 金山徳志. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
